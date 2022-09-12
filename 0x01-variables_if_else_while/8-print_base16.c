f524f870b5a2db015567b360d15515e466be1c15
SamiA86 <samueljunk86@gmail.com>
2022-09-12 09:00:07 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - printing numbers from 0-9 with commas and space between them
+ *
+ * Description: using the main function
+ * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+int d;
+c = 'a';
+
+while (d < 10) 
+{
+putchar(d+'0');
+d++
+}
+
+while (c <= 'f')
+{
+putchar(c);
+c++
+}
+putchar('\n');
+return (0);
+}
