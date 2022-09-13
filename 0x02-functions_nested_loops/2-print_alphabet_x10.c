36c178ae5e2ccd0f1855dfdf98e228317856b8a8
SamiA86 <samueljunk86@gmail.com>
2022-09-13 04:44:57 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - print alphabet 10x
+ * 
+ * Description: using the main function
+ * this program prints "Print alpha bet 10x
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+int j;
+int x;
+for (x = 0; x < 10; x++)
+}
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+
