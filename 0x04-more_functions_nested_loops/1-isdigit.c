0873680ae0ecb74f696a540455a223ce7ce34eb7
SamiA86 <samueljunk86@gmail.com>
2022-09-15 02:46:28 -0700

    hf

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _islower -> checks if the character is lowercase
- *
- * @c: a charcter argument
- * Return: returns 1 and 0 depending on condition
- */
+* _isdigit -> checks if the character is digit
+*
+* @c: a charcter argument
+* Return: returns 1 and 0 depending on condition
+*/
 int _isdigit(int c)
 {
+
 return (c >= '0' && c <= '9');
 }
