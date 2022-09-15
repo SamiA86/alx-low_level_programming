7b4339097495d5f74bdd5e4235a2fc8d408676ff
SamiA86 <samueljunk86@gmail.com>
2022-09-15 02:39:58 -0700

    is digit added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _islower -> checks if the character is lowercase
+ *
+ * @c: a charcter argument
+ * Return: returns 1 and 0 depending on condition
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
