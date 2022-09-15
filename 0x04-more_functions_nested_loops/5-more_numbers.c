284c4138679973a9b186ed38b28c252490bcdae9
SamiA86 <samueljunk86@gmail.com>
2022-09-15 04:16:31 -0700

    0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-14 10x
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+i = 0;
+j = 0;
+
+for (i = 0; i < 11; i++)
+{
+for (j = 0; i < 15; j++)	
+{
+
+_putchar(i +'0');
+}
+
+_putchar('\n');
+}
+}
