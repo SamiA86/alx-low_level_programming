098d0276a8f0ef7b717ed6bf93d22e9f5185dd0a
SamiA86 <samueljunk86@gmail.com>
2022-09-15 05:50:30 -0700

    digonl

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_diagonal - prints a diagonal
+ *
+ *@n: parameter
+ *Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+int l, s;
+if (n > 0)
+{
+for (l = 0; l < n; l++)
+{
+for (s = 0; s < l; s++)
+{
+_putchar(' ');
+}
+_putchar(92);
+if (l == (n - 1))
+{
+continue;
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
