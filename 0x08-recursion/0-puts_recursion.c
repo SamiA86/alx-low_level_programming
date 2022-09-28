9f1b9f978812d5ec5a4488957e2346e2cb44fffc
SamiA86 <samueljunk86@gmail.com>
2022-09-28 00:04:50 -0700

    hf

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@ void _puts_recursion(char *s)
 int i = 0;
 _putchar(s[i]);
 i--;
-if (s[i] == '\0') 
+if (*(s + i) == '\0') 
 {
 _putchar('\n');
-retrun;
+return;
 }
-_puts_recursion(s[i]);
+_puts_recursion(*(s + i));
 }
 
