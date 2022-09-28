ad6daf0a64f61c43011ea72ca53b0df51c81f81e
SamiA86 <samueljunk86@gmail.com>
2022-09-28 00:30:23 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,14 @@
  *
  * @s: string paramenter
  */
-
 void _puts_recursion(char *s)
 {
-_putchar(*s);
-++s;
 if (*s == '\0')
+{
 _putchar('\n');
 return;
 }
+_putchar(*s);
+s++;
 _puts_recursion(s);
 }
-
