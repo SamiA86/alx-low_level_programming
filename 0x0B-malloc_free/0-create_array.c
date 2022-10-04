18d5a5f0ce54ba27221be8cab953a6e2ef45c00b
SamiA86 <samueljunk86@gmail.com>
2022-10-03 22:06:42 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *create_array -> creates array and intializes
- * @size arrgument for size of the array
- * @c arrgumetn for initialize character
- * return a pointer to dynamically allocated array
+ *create_array-> creates array and intializes
+ * @size: arrgument for size of the array
+ * @c:arrgumetn for initialize character
+ * Return: a pointer to dynamically allocated array
  */
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
-char* sz = (char *) malloc(size * sizeof(char));
+char *sz = (char *) malloc(size * sizeof(char));
 if (size == 0)
 return (NULL);
 if (sz == NULL)
