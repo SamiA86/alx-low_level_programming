8840412ed7f6d4a97849ec9d81adc34b541a82e0
SamiA86 <samueljunk86@gmail.com>
2022-10-04 03:07:15 -0700

    string conc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,33 +8,33 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int len = 0, j, i;
-char *NS;
+int i = 0, j = 0, l = 0, k = 0;
+char *s;
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
-while (s1[len])
-len++;
-for (i = 0; s2[i] != 0; i++)
-{
-s1[len] = s2[i];
-len += 1;
-}
-NS = (char *)malloc(sizeof(char) * len + 1);
-if (NS == NULL)
-{
+while (s1[i])
+i++;
+while (s2[j])
+j++;
+l = i + j;
+s = (char *)malloc(l * sizeof(char) + 1);
+if (s == NULL)
 return (NULL);
-}
-for (j = 0; j < len + 1; j++)
+j = 0;
+while (k < l)
 {
-	NS[j] = s1[j];
+if (k < i)
+s[k] = s1[k];
+if (k >= i)
+{
+s[k] = s2[j];
+j++;
+}
+k++;
+}
+s[k] = '\0';
+return (s);
 }
 
-NS[j] = '\0';
-return (NS);
-}
