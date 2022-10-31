c0dce741a99108f42d855b0ddfc89b245f0e98b9
SamiA86 <samueljunk86@gmail.com>
2022-10-31 05:03:07 -0700

    read textfile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
 #include <elf.h>
 #include <stdlib.h>
@@ -14,4 +14,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 
-#endif 
+#endif
